388b4fb10eca4837695be20b6d68cc79cbd33544
liuweimin <liuweimin@corp.netease.com>
2018-12-14 13:44:10 +0800

    custom react cli

---
+++ config/webpack.base.conf.js
@@ -0,0 +1,64 @@
+'use strict';
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: {
+		app: './src/main.js'
+	},
+	output: {
+		path: path.resolve(__dirname, '../dist'),
+		filename: "[name].js"
+	},
+	resolve: {
+		extensions: ['.ts', '.tsx', '.js', '.json']
+	},
+	module: {
+		rules: [{
+			test: /\.js|jsx$/,
+			exclude: /(node_modules|bower_components)/,
+			loader: 'babel-loader'
+		}, {
+			test: /\.scss$/,
+			use: [{
+				loader: "style-loader"
+			}, {
+				loader: "css-loader"
+			}, {
+				loader: "sass-loader"
+			}]
+		}, {
+			test: /\.js[x]?$/,
+			enforce: 'pre',
+			use: [{
+				loader: 'eslint-loader',
+				options: {
+					fix: true
+				}
+			}],
+			include: path.resolve(__dirname, './src/**/*.js'),
+			exclude: /node_modules/
+		}, {
+			test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+			loader: 'url-loader',
+			query: {
+				limit: 30000,
+				name: 'images/[name].[ext]?[hash]'
+			}
+		}, {
+			test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+			loader: "url-loader",
+			options: {
+				limit: 10000,
+				name: 'font/[name].[ext]?[hash]'
+			}
+		}]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			template: 'index.html',
+			inject: 'body'
+		})
+	]
+};
---
+++ src/routes/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import avatar from '../assets/images/avator.png';
+
+const App = () => {
+	return (
+		<img src={avatar} />
+	);
+};
+
+export default App;
